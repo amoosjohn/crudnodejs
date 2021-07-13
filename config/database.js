9b85bc6516c07e821620e439e82b19f6447bf29d
Amoos <amoos.ghori@bargoventures.com>
2021-07-13 19:47:50 +0500

    'changes'

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
 	'connection': {
-		'host':'localhost',
-		'user':'root',
-		'password':''
+		'host':'us-cdbr-east-04.cleardb.com',
+		'user':'b2d9c7e960c28c',
+		'password':'04d0654b'
 	},
-	'database' : 'nodejs',
-	'user_tab;e' : 'users'
+	'database' : 'heroku_4bf20b93ecd3ec1',
+	'user_table' : 'users'
 };
